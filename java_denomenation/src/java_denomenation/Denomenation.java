177de0c145c1415e52e3eee7d17ef877e9fb8b64
rbandong041396 <58019012+rbandong041396@users.noreply.github.com>
2020-01-12 23:29:47 -0800

    Adding BufferedReader

--- java_denomenation/src/java_denomenation/Denomenation.java
+++ java_denomenation/src/java_denomenation/Denomenation.java
@@ -1,10 +1,14 @@
 package java_denomenation;
-
+import java.io.*;
 public class Denomenation {
-
-	public static void main(String[] args) {
+	
+	public static void main(String[] args)throws Exception{
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+		System.out.print("Enter your name: ");
+		String name = in.readLine();
+		
+		System.out.println("Hello " + name);
 
 	}
 
