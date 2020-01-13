75c00b98d727011bff93c5ea6844e856cf20bae1
rbandong041396 <58019012+rbandong041396@users.noreply.github.com>
2020-01-12 18:22:16 -0800

    This is for FIRST COMMIT

---
+++ java_denomenation/src/java_denomenation/Denomenation.java
@@ -0,0 +1,11 @@
+package java_denomenation;
+
+public class Denomenation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+
+}
